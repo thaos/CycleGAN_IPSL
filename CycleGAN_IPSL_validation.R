a3747507464865f7ce121ff49608c2c0d555e325
thaos <sthao@lsce.ipsl.fr>
2020-02-20 11:13:45 +0100

    validation script

---
+++ CycleGAN_IPSL_validation.R
@@ -0,0 +1,309 @@
+library(reticulate)
+library(magrittr)
+library(fields)
+require (maps)
+library(RColorBrewer)
+
+oldpar = par()
+resfactor = 2
+world = map("world", plot = FALSE)
+
+datasetA  <- py_load_object("datasetA.npy") %>% drop	
+datasetB  <- py_load_object("datasetB.npy") %>% drop	
+valsetA  <- py_load_object("valsetA.npy") %>% drop	
+valsetB  <- py_load_object("valsetB.npy") %>% drop	
+histsetA  <- py_load_object("histsetA.npy") %>% drop	
+histsetB  <- py_load_object("histsetB.npy") %>% drop	
+rcpsetA  <- py_load_object("rcpsetA.npy") %>% drop	
+rcpsetB  <- py_load_object("rcpsetB.npy") %>% drop	
+fakesetB  <- py_load_object("fakesetB.npy") %>% drop	
+fakevalB  <- py_load_object("fakevalB.npy") %>% drop	
+fakehistB  <- py_load_object("fakehistB.npy") %>% drop	
+fakercpB  <- py_load_object("fakercpB.npy") %>% drop	
+lon  <- py_load_object("lon.npy") %>% drop	
+lat  <- py_load_object("lat.npy") %>% drop	
+
+qqsetB <- datasetA
+for(i in seq.int(dim(qqsetB)[2])){
+  for(j in seq.int(dim(qqsetB)[3])){
+    qqsetB[, i, j] <- quantile(datasetB[, i, j], probs = ecdf(datasetA[, i, j])(datasetA[, i, j]))
+  }
+}
+qqvalB <- valsetA
+for(i in seq.int(dim(qqsetB)[2])){
+  for(j in seq.int(dim(qqsetB)[3])){
+    qqvalB[, i, j] <- quantile(datasetB[, i, j], probs = ecdf(datasetA[, i, j])(valsetA[, i, j]))
+  }
+}
+qqhistB <- histsetA
+for(i in seq.int(dim(qqsetB)[2])){
+  for(j in seq.int(dim(qqsetB)[3])){
+    qqhistB[, i, j] <- quantile(datasetB[, i, j], probs = ecdf(datasetA[, i, j])(histsetA[, i, j]))
+  }
+}
+qqrcpB <- rcpsetA
+for(i in seq.int(dim(qqsetB)[2])){
+  for(j in seq.int(dim(qqsetB)[3])){
+    qqrcpB[, i, j] <- quantile(datasetB[, i, j], probs = ecdf(datasetA[, i, j])(rcpsetA[, i, j]))
+  }
+}
+print("***********************************************")
+print("Scores on calibration set")
+print("***********************************************")
+sprintf('bias genA2B: %f', mean((fakesetB - datasetB)))
+sprintf('bias qqA2B : %f', mean((qqsetB - datasetB)))
+sprintf('bias base A: %f', mean((datasetA - datasetB)))
+sprintf('rmse genA2B: %f', mean((fakesetB - datasetB)^2) %>% sqrt)
+sprintf('rmse qqA2B : %f', mean((qqsetB - datasetB)^2) %>% sqrt)
+sprintf('rmse base A: %f', mean((datasetA - datasetB)^2) %>% sqrt)
+sprintf('corr genA2B: %f', cor(c(fakesetB), c(datasetB)))
+sprintf('corr qqA2B : %f', cor(c(qqsetB), c(datasetB)))
+sprintf('corr baseA: %f', cor(c(datasetA), c(datasetB)))
+
+print("***********************************************")
+print("Scores on validation set")
+print("***********************************************")
+sprintf('bias genA2B: %f', mean((fakevalB - valsetB)))
+sprintf('bias qqA2B : %f', mean((qqvalB - valsetB)))
+sprintf('bias base A: %f', mean((valsetA - valsetB)))
+sprintf('rmse genA2B: %f', mean((fakevalB - valsetB)^2) %>% sqrt)
+sprintf('rmse qqA2B : %f', mean((qqvalB - valsetB)^2) %>% sqrt)
+sprintf('rmse base A: %f', mean((valsetA - valsetB)^2) %>% sqrt)
+sprintf('corr genA2B: %f', cor(c(fakevalB), c(valsetB)))
+sprintf('corr qqA2B : %f', cor(c(qqvalB), c(valsetB)))
+sprintf('corr baseA: %f', cor(c(valsetA), c(valsetB)))
+
+print("***********************************************")
+print("Scores on historical set")
+print("***********************************************")
+sprintf('bias genA2B: %f', mean((fakehistB - histsetB)))
+sprintf('bias qqA2B : %f', mean((qqhistB - histsetB)))
+sprintf('bias base A: %f', mean((histsetA - histsetB)))
+sprintf('rmse genA2B: %f', mean((fakehistB - histsetB)^2) %>% sqrt)
+sprintf('rmse qqA2B : %f', mean((qqhistB - histsetB)^2) %>% sqrt)
+sprintf('rmse base A: %f', mean((histsetA - histsetB)^2) %>% sqrt)
+sprintf('corr genA2B: %f', cor(c(fakehistB), c(histsetB)))
+sprintf('corr qqA2B : %f', cor(c(qqhistB), c(histsetB)))
+sprintf('corr baseA: %f', cor(c(histsetA), c(histsetB)))
+
+print("***********************************************")
+print("Scores on rcp set")
+print("***********************************************")
+sprintf('bias genA2B: %f', mean((fakercpB - rcpsetB)))
+sprintf('bias qqA2B : %f', mean((qqrcpB - rcpsetB)))
+sprintf('bias base A: %f', mean((rcpsetA - rcpsetB)))
+sprintf('rmse genA2B: %f', mean((fakercpB - rcpsetB)^2) %>% sqrt)
+sprintf('rmse qqA2B : %f', mean((qqrcpB - rcpsetB)^2) %>% sqrt)
+sprintf('rmse base A: %f', mean((rcpsetA - rcpsetB)^2) %>% sqrt)
+sprintf('corr genA2B: %f', cor(c(fakercpB), c(rcpsetB)))
+sprintf('corr qqA2B : %f', cor(c(qqrcpB), c(rcpsetB)))
+sprintf('corr baseA: %f', cor(c(rcpsetA), c(rcpsetB)))
+
+paltas  <- colorRampPalette(brewer.pal(9, name = "YlGnBu"))(31)
+paldiff  <- colorRampPalette(rev(brewer.pal(11, name = "RdYlBu")))(31)
+
+meanA  <- t(apply(datasetA, 2:3, mean))
+meanqqB  <- t(apply(qqsetB, 2:3, mean))
+meanfakeB  <- t(apply(fakesetB, 2:3, mean))
+meanB  <- t(apply(datasetB, 2:3, mean))
+zlim  <- range(meanA, meanqqB, meanfakeB, meanB)
+png("mean_cal.png", res = 72*resfactor, width = resfactor * 720, height = resfactor * 720)
+par(mfrow = c(2, 2))
+par(mar=c(5, 5, 5, 8))
+image.plot(lon, lat, meanA, zlim = zlim, col = paltas, main = "mean A")
+lines(world) 
+image.plot(lon, lat, meanqqB, zlim = zlim, col = paltas, main = "mean qqB")
+lines(world) 
+image.plot(lon, lat, meanfakeB, zlim = zlim, col = paltas, main = "mean ganB")
+lines(world) 
+image.plot(lon, lat, meanB, zlim = zlim, col = paltas, main = "mean B")
+lines(world) 
+dev.off()
+
+png("diffmean_cal.png", res = 72*resfactor, width = resfactor * 720, height = resfactor *  720)
+zlim  <- range(c(meanA, meanqqB, meanfakeB) - c(meanB)) %>% abs %>% max %>% "*"(., c(-1, 1))
+par(mfrow = c(2, 2))
+par(mar=c(5, 5, 5, 8))
+image.plot(lon, lat, meanA - meanB, zlim = zlim, col = paldiff, main = "mean A-B")
+lines(world) 
+image.plot(lon, lat, meanqqB - meanB , zlim = zlim, col = paldiff, main = "mean qqB-B")
+lines(world) 
+image.plot(lon, lat, meanfakeB - meanB, zlim = zlim, col = paldiff, main = "mean ganB-B")
+lines(world) 
+image.plot(lon, lat, meanB - meanB, zlim = zlim, col = paldiff, main = "mean B-B")
+lines(world) 
+dev.off()
+
+sdA  <- t(apply(datasetA, 2:3, sd))
+sdqqB  <- t(apply(qqsetB, 2:3, sd))
+sdfakeB  <- t(apply(fakesetB, 2:3, sd))
+sdB  <- t(apply(datasetB, 2:3, sd))
+zlim  <- range(sdA, sdqqB, sdfakeB, sdA)
+png("sd_cal.png", res = 72*resfactor, width = resfactor * 720, height = resfactor * 720)
+par(mfrow = c(2, 2))
+par(mar=c(5, 5, 5, 8))
+image.plot(lon, lat, sdA, zlim = zlim, col = paltas, main = "sd A")
+lines(world) 
+image.plot(lon, lat, sdqqB, zlim = zlim, col = paltas, main = "sd qqB")
+lines(world) 
+image.plot(lon, lat, sdfakeB, zlim = zlim, col = paltas, main = "sd ganB")
+lines(world) 
+image.plot(lon, lat, sdB, zlim = zlim, col = paltas, main = "sd B")
+lines(world) 
+dev.off()
+
+zlim  <- range(c(sdA, sdqqB, sdfakeB) - c(sdB)) %>% abs %>% max %>% "*"(., c(-1, 1))
+png("diffsd_cal.png", res = 72*resfactor, width = resfactor * 720, height = resfactor * 720)
+par(mfrow = c(2, 2))
+par(mar=c(5, 5, 5, 8))
+image.plot(lon, lat, sdA - sdB, zlim = zlim, col = paldiff, main = "sd A - sd B")
+lines(world) 
+image.plot(lon, lat, sdqqB - sdB , zlim = zlim, col = paldiff, main = "sd qqB - sd B")
+lines(world) 
+image.plot(lon, lat, sdfakeB - sdB, zlim = zlim, col = paldiff, main = "sd ganB - sd B")
+lines(world) 
+image.plot(lon, lat, sdB - sdB, zlim = zlim, col = paldiff, main = "sd B - sd B")
+lines(world) 
+dev.off()
+
+spatial_rmse_fakercpB  <- apply( (fakercpB - rcpsetB)^2, 1, function(x)sqrt(mean(x)))
+spatial_rmse_qqrcpB  <- apply( (qqrcpB - rcpsetB)^2, 1, function(x)sqrt(mean(x)))
+ylim  <- range(spatial_rmse_qqrcpB, spatial_rmse_fakercpB) 
+png("spatial_rmse.png", res = 72*resfactor, width = resfactor * 1280, height = resfactor * 480)
+par(mfrow = c(1, 3))
+par(mar=c(5, 5, 5, 8))
+plot(spatial_rmse_fakercpB, pch = 19, ylim = ylim, main = "spatial: rmse gan B")
+plot(spatial_rmse_qqrcpB, pch = 19, ylim = ylim, main = "spatial: rmse qq B")
+plot(spatial_rmse_fakercpB - spatial_rmse_qqrcpB, pch = 19, main = "spatial: rmse gan B -  rmse qq B")
+dev.off()
+
+temporal_rmse_fakercpB  <- apply( (fakercpB - rcpsetB)^2, 2:3, function(x)sqrt(mean(x)))
+temporal_rmse_qqrcpB  <- apply( (qqrcpB - rcpsetB)^2, 2:3, function(x)sqrt(mean(x)))
+
+zlim_diff  <- range(temporal_rmse_qqrcpB - temporal_rmse_fakercpB) %>% abs %>% max %>% "*"(., c(-1, 1))
+zlim  <- range(temporal_rmse_qqrcpB, temporal_rmse_fakercpB) 
+png("temporal_rmse.png", res = 72*resfactor, width = resfactor * 1280, height = resfactor * 480)
+par(mfrow = c(1, 3))
+par(mar=c(5, 5, 5, 8))
+image.plot(lon, lat, temporal_rmse_fakercpB, pch = 20, zlim = zlim, col = paltas, main = "temporal: rmse ganB")
+lines(world)
+image.plot(lon, lat, temporal_rmse_qqrcpB, pch = 20, zlim = zlim, col = paltas, main = "temporal: rmse qqB")
+lines(world)
+image.plot(lon, lat, temporal_rmse_fakercpB - temporal_rmse_qqrcpB, pch = 20, zlim = zlim_diff, col = paldiff, main = "temporal: rmse gan B -  rmse qq B")
+lines(world)
+dev.off()
+
+spatial_corr_fakercpB  <- fakercpB[, 1, 1]
+spatial_corr_qqrcpB  <- fakercpB[, 1, 1]
+for(i in 1:nrow(fakercpB)){
+  spatial_corr_fakercpB[i] = cor(c(fakercpB[i,,]), c(rcpsetB[i,,]))
+  spatial_corr_qqrcpB[i] = cor(c(qqrcpB[i,,]), c(rcpsetB[i,,]))
+}
+ylim  <- range(spatial_corr_qqrcpB, spatial_corr_fakercpB) 
+png("spatial_corr.png", res = 72*resfactor, width = resfactor * 1280, height = resfactor * 480)
+par(mfrow = c(1, 3))
+par(mar=c(5, 5, 5, 8))
+plot(spatial_corr_fakercpB, pch = 19, ylim = ylim, main = "spatial: corr gan B")
+plot(spatial_corr_qqrcpB, pch = 19, ylim = ylim, main = "spatial: corr qq B")
+plot(spatial_corr_fakercpB - spatial_corr_qqrcpB, pch = 19, main = "spatial: corr gan B -  corr qqB")
+dev.off()
+
+temporal_corr_fakercpB  <- fakercpB[1, , ]
+temporal_corr_qqrcpB  <- fakercpB[1, , ]
+for(i in 1:nrow(temporal_corr_qqrcpB)){
+  for(j in 1:ncol(temporal_corr_qqrcpB)){
+    temporal_corr_fakercpB[i, j] = cor(fakercpB[,i,j], rcpsetB[,i,j])
+    temporal_corr_qqrcpB[i, j] = cor(qqrcpB[,i,j], rcpsetB[,i,j])
+  }
+}
+
+zlim_diff  <- range(temporal_corr_qqrcpB - temporal_corr_fakercpB) %>% abs %>% max %>% "*"(., c(-1, 1))
+zlim  <- range(temporal_corr_qqrcpB, temporal_corr_fakercpB) 
+png("temporal_corr.png", res = 72*resfactor, width = resfactor * 1280, height = resfactor * 480)
+par(mfrow = c(1, 3))
+par(mar=c(5, 5, 5, 8))
+image.plot(lon, lat, temporal_corr_fakercpB, pch = 20, zlim = zlim, col = paltas, main = "temporal: corr ganB")
+lines(world)
+image.plot(lon, lat, temporal_corr_qqrcpB, pch = 20, zlim = zlim, col = paltas, main = "temporal: corr qqB")
+lines(world)
+image.plot(lon, lat, temporal_corr_fakercpB - temporal_corr_qqrcpB, pch = 20, zlim = zlim_diff, col = paldiff, main = "temporal: corr gan B -  corr qqB")
+lines(world)
+dev.off()
+
+# zlim  <- range(c(qqsetB, fakesetB) - c(datasetB)) %>% abs %>% max %>% "*"(., c(-1, 1))
+# for (i in 1:nrow(datasetA)) {
+#   filename = sprintf("img/cal%05d.png", i )
+#   png(filename, width = 1280, height = 480)
+# draw your plots here, then pause for a while with
+#   A  <- t(datasetA[i,,])
+#   qqB  <- t(qqsetB[i,,])
+#   fakeB  <- t(fakesetB[i,,])
+#   B  <- t(datasetB[i,,])
+#   par(mfrow = c(1, 3))
+#   image.plot(lon, lat, B, zlim = c(0, 1), main = "B", col = paltas)
+#   lines(world) 
+#   image.plot(lon, lat, qqB - B, zlim = zlim, main = "qqB - B", col = paldiff)
+#   lines(world) 
+#   image.plot(lon, lat, fakeB - B, zlim = zlim, main = "ganB - B", col = paldiff)
+#   lines(world) 
+#   dev.off()
+# }
+# ffmpeg -i img/val%05d.png -b:v 8000k   -c:v libx264  -s:v 1280x720  video.mp4
+# 
+# zlim  <- range(c(qqvalB, fakevalB) - c(valsetB)) %>% abs %>% max %>% "*"(., c(-1, 1))
+# for (i in 1:nrow(valsetA)) {
+#   filename = sprintf("img/val%05d.png", i )
+#   png(filename, width = 1280, height = 480)
+# draw your plots here, then pause for a while with
+#   A  <- t(valsetA[i,,])
+#   qqB  <- t(qqvalB[i,,])
+#   fakeB  <- t(fakevalB[i,,])
+#   B  <- t(valsetB[i,,])
+#   par(mfrow = c(1, 3))
+#   image.plot(lon, lat, B, zlim = c(0, 1), main = "B", col = paltas)
+#   lines(world) 
+#   image.plot(lon, lat, qqB - B, zlim = zlim, main = "qqB - B", col = paldiff)
+#   lines(world) 
+#   image.plot(lon, lat, fakeB - B, zlim = zlim, main = "ganB - B", col = paldiff)
+#   lines(world) 
+#   dev.off()
+# }
+# 
+# zlim  <- range(c(qqhistB, fakehistB) - c(histsetB)) %>% abs %>% max %>% "*"(., c(-1, 1))
+# for (i in 1:nrow(histsetA)) {
+#   filename = sprintf("img/hist%05d.png", i )
+#   png(filename, width = 1280, height = 480)
+# draw your plots here, then pause for a while with
+#   A  <- t(histsetA[i,,])
+#   qqB  <- t(qqhistB[i,,])
+#   fakeB  <- t(fakehistB[i,,])
+#   B  <- t(histsetB[i,,])
+#   par(mfrow = c(1, 3))
+# image.plot(lon, lat, A - B, zlim = zlim, main = "A - B", col = paldiff)
+# lines(world) 
+#   image.plot(lon, lat, B, zlim = c(0, 1), main = "B", col = paltas)
+#   lines(world) 
+#   image.plot(lon, lat, qqB - B, zlim = zlim, main = "qqB - B", col = paldiff)
+#   lines(world) 
+#   image.plot(lon, lat, fakeB - B, zlim = zlim, main = "ganB - B", col = paldiff)
+#   lines(world) 
+#   dev.off()
+# }
+# 
+# zlim  <- range(c(qqrcpB, fakercpB) - c(rcpsetB)) %>% abs %>% max %>% "*"(., c(-1, 1))
+# for (i in 1:nrow(rcpsetA)) {
+#   filename = sprintf("img/rcp%05d.png", i )
+#   png(filename, width = 1280, height = 480)
+#   A  <- t(rcpsetA[i,,])
+#   qqB  <- t(qqrcpB[i,,])
+#   fakeB  <- t(fakercpB[i,,])
+#   B  <- t(rcpsetB[i,,])
+#   par(mfrow = c(1, 3))
+#   image.plot(lon, lat, B, zlim = c(0, 1), main = "B", col = paltas)
+#   lines(world) 
+#   image.plot(lon, lat, qqB - B, zlim = zlim, main = "qqB - B", col = paldiff)
+#   lines(world) 
+#   image.plot(lon, lat, fakeB - B, zlim = zlim, main = "ganB - B", col = paldiff)
+#   lines(world) 
+#   dev.off()
+# }
--- CycleGAN_IPSL_validation.py
+++ CycleGAN_IPSL_validation.py
@@ -26,6 +26,40 @@ nimage = min(datasetA.shape[0], datasetB.shape[0])
 datasetA = datasetA[range(nimage)]
 datasetB = datasetB[range(nimage)]
 
+dataset = Dataset('tas_day_IPSL-CM5A-MR_piControl_r1i1p1_18000101-18191231_coarsegrain_europe.nc', "r", format="NETCDF4")
+XA = dataset.variables["tas"]
+XA = expand_dims(XA, axis=-1)
+XA = XA.astype('float32')
+XminA = XA.min()
+XmaxA = XA.max()
+
+dataset = Dataset('tas_day_IPSL-CM5A-MR_piControl_r1i1p1_18000101-18191231_europe.nc', "r", format="NETCDF4")
+XB = dataset.variables["tas"]
+XB = expand_dims(XB, axis=-1)
+XB = XB.astype('float32')
+XminB = XB.min()
+XmaxB = XB.max()
+
+def load_nc_samples(ncfile, Xmin, Xmax):
+    # load mnist dataset
+    dataset = Dataset(ncfile, "r", format="NETCDF4")
+    lon = dataset.variables["lon"]
+    lat = dataset.variables["lat"]
+    X = dataset.variables["tas"]
+    # expand to 3d, e.g. add channels dimension
+    X = expand_dims(X, axis=-1)
+    # convert from unsigned ints to floats
+    X = X.astype('float32')
+    # scale from [0,255] to [0,1]
+    X = (X - Xmin) / (Xmax - Xmin)
+    return X, lon, lat
+valsetA, lon, lat = load_nc_samples('tas_day_IPSL-CM5A-MR_piControl_r1i1p1_20000101-20191231_coarsegrain_europe.nc', XminA, XmaxA)
+valsetB, lon, lat = load_nc_samples('tas_day_IPSL-CM5A-MR_piControl_r1i1p1_20000101-20191231_europe.nc', XminB, XmaxB)
+histsetA, lon, lat = load_nc_samples('tas_day_IPSL-CM5A-MR_historical_r1i1p1_19500101-19991231_coarsegrain_europe.nc', XminA, XmaxA)
+histsetB, lon, lat = load_nc_samples('tas_day_IPSL-CM5A-MR_historical_r1i1p1_19500101-19991231_europe.nc', XminB, XmaxB)
+rcpsetA, lon, lat = load_nc_samples('tas_day_IPSL-CM5A-MR_rcp85_r1i1p1_20560101-21001231_coarsegrain_europe.nc', XminA, XmaxA)
+rcpsetB, lon, lat = load_nc_samples('tas_day_IPSL-CM5A-MR_rcp85_r1i1p1_20560101-21001231_europe.nc', XminB, XmaxB)
+
 print(datasetA.shape)
 for layer in genA2B.layers:
     print(layer.input_shape)
@@ -34,6 +68,9 @@ print(genA2B.summary())
 
 # Validation with real pairs
 fakesetB = genA2B.predict(datasetA)
+fakevalB = genA2B.predict(valsetA)
+fakehistB = genA2B.predict(histsetA)
+fakercpB = genA2B.predict(rcpsetA)
 nchecks = 4 
 datasubA = datasetA[range(nchecks)]
 datasubB = datasetB[range(nchecks)]
@@ -104,33 +141,42 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from matplotlib import animation
 
-fig, axes = pyplot.subplots(nrows = 1, ncols = 3, sharex = True, sharey = True)
-
-frame = 0
-axes[0].set_title("A")
-map.pcolormesh(xx,yy, datasetA[frame, :, :, 0], vmin = 0, vmax = 1, cmap=cmap, ax=axes[0])
-map.drawcoastlines(linewidth = 0.2, ax=axes[0])
-axes[1].set_title("B")
-map.pcolormesh(xx,yy, datasetB[frame, :, :, 0], vmin = 0, vmax = 1, cmap=cmap, ax=axes[1])
-map.drawcoastlines(linewidth = 0.2, ax=axes[1])
-axes[2].set_title("genA2B(A)")
-map.pcolormesh(xx,yy, fakesetB[frame, :, :, 0], vmin = 0, vmax = 1, cmap=cmap, ax=axes[2])
-map.drawcoastlines(linewidth = 0.2, ax=axes[2])
-#pyplot.show()
+fig, axes = pyplot.subplots(nrows = 2, ncols = 3, sharex = True, sharey = True)
 
 def update(frame):
-    axes[0].set_title("A")
-    ln1 = map.pcolormesh(xx,yy, datasetA[frame, :, :, 0], vmin = 0, vmax = 1, cmap=cmap, ax=axes[0])
-    map.drawcoastlines(linewidth = 0.2, ax=axes[0])
-    axes[1].set_title("B")
-    ln2 = map.pcolormesh(xx,yy, datasetB[frame, :, :, 0], vmin = 0, vmax = 1, cmap=cmap, ax=axes[1])
-    map.drawcoastlines(linewidth = 0.2, ax=axes[1])
-    axes[2].set_title("genA2B(A)")
-    ln3 = map.pcolormesh(xx,yy, fakesetB[frame, :, :, 0], vmin = 0, vmax = 1, cmap=cmap, ax=axes[2])
-    map.drawcoastlines(linewidth = 0.2, ax=axes[2])
-    return [ln1, ln2, ln3]
+    axes[0, 0].set_title("A")
+    ln1 = map.pcolormesh(xx,yy, datasetA[frame, :, :, 0], vmin = 0, vmax = 1, cmap=cmap, ax=axes[0, 0])
+    map.drawcoastlines(linewidth = 0.2, ax=axes[0, 0])
+    axes[0, 1].set_title("B")
+    ln2 = map.pcolormesh(xx,yy, datasetB[frame, :, :, 0], vmin = 0, vmax = 1, cmap=cmap, ax=axes[0, 1])
+    map.drawcoastlines(linewidth = 0.2, ax=axes[0, 1])
+    axes[0, 2].set_title("genA2B(A)")
+    ln3 = map.pcolormesh(xx,yy, fakesetB[frame, :, :, 0], vmin = 0, vmax = 1, cmap=cmap, ax=axes[0, 2])
+    map.drawcoastlines(linewidth = 0.2, ax=axes[0, 2])
+    axes[1, 0].set_title("A - B")
+    ln4 = map.pcolormesh(xx,yy, datasetA[frame, :, :, 0] - datasetB[frame, :, :, 0], vmin = -0.1, vmax = 0.1, cmap=cmap, ax=axes[1, 0])
+    map.drawcoastlines(linewidth = 0.2, ax=axes[1, 0])
+    axes[1, 2].set_title("genA2B(A) - B")
+    ln5 = map.pcolormesh(xx,yy, fakesetB[frame, :, :, 0] - datasetB[frame, :, :, 0], vmin = -0.1, vmax = 0.1, cmap=cmap, ax=axes[1, 2])
+    map.drawcoastlines(linewidth = 0.2, ax=axes[1, 2])
+    return [ln1, ln2, ln3, ln4, ln5]
 
-ani = FuncAnimation(fig, update, frames=range(100), blit=False)
+#ani = FuncAnimation(fig, update, frames=range(3 * 365), blit=False)
 #pyplot.show()
-writer = animation.writers['ffmpeg'](fps=30)
-ani.save('demo.mp4',writer=writer,dpi=150)
+#writer = animation.writers['ffmpeg'](fps=30)
+#ani.save('demo.mp4',writer=writer,dpi=150)
+
+datasetA.dump("datasetA.npy")
+datasetB.dump("datasetB.npy")
+valsetA.dump("valsetA.npy")
+valsetB.dump("valsetB.npy")
+histsetA.dump("histsetA.npy")
+histsetB.dump("histsetB.npy")
+rcpsetA.dump("rcpsetA.npy")
+rcpsetB.dump("rcpsetB.npy")
+fakesetB.dump("fakesetB.npy")
+fakevalB.dump("fakevalB.npy")
+fakehistB.dump("fakehistB.npy")
+fakercpB.dump("fakercpB.npy")
+np.array(lon).dump("lon.npy")
+np.array(lat).dump("lat.npy")
